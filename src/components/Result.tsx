9382c836f8fdd9642504e8f075887d3fa974062d
eeren3411 <eeren3411@gmail.com>
2025-05-04 23:24:28 +0300

    Result component

---
+++ src/components/Result.tsx
@@ -0,0 +1,28 @@
+import 'react';
+
+import { QRCodeCanvas } from 'qrcode.react';
+import { FaCopy } from 'react-icons/fa6';
+
+export type ResultProps = {
+	url: string
+}
+
+function Result({ url }: ResultProps) {
+	return (
+		<div className="result_wrapper">
+			<div className="result_container">
+				<div className="result">
+					<a className="result_link" href={url}>
+						{url}
+					</a>
+				</div>
+				<button className="copy_button">
+					<FaCopy />
+				</button>
+			</div>
+			<QRCodeCanvas value={url} size={190}/>
+		</div>
+	)
+}
+
+export default Result
