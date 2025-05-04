c19c32246460974edfa472ed3805f31d5e33458d
eeren3411 <eeren3411@gmail.com>
2025-05-04 23:25:12 +0300

    Add basic app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
 
+import Result from './components/Result'
+
 function App() {
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState("")
 
   return (
-    <h1>Hello, World!</h1>
+    <div className="app">
+      <h1>URL Shortener</h1>
+      <div className="input_container">
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button
+          className="submit_button"
+          onClick={() => setResult(url)}
+        >
+          Submit
+        </button>
+      </div>
+      {result && <Result url={result} />}
+    </div>
   )
 }
 
