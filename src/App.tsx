cd6e0a93c8a0d488f8e5a626353c172b2cdbf1ca
eeren3411 <eeren3411@gmail.com>
2025-05-07 20:08:12 +0300

    Add theme selector
    Co-authored-by: İdil Patir <idilpatir@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Result from './components/Result'
 
+import { IoSunny } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
+
+
 function App() {
   const [url, setUrl] = useState("");
-  const [result, setResult] = useState("")
+  const [result, setResult] = useState("");
+  const [useDark, setUseDark] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+  useEffect(() => {
+    if (useDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [useDark]);
+
 
   return (
-    <div className="app">
-      <h1>URL Shortener</h1>
-      <div className="input_container">
-        <input
-          type="text"
-          className="input"
-          placeholder="Enter URL"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <button
-          className="submit_button"
-          onClick={() => setResult(url)}
-        >
-          Submit
-        </button>
+    <>
+      <IoSunny size="32" className={`theme_selector ${useDark ? 'fade-in' : 'fade-out'}`} onClick={() => setUseDark(false)}/>
+      <FaMoon size="32" className={`theme_selector ${useDark ? 'fade-out' : 'fade-in'}`} onClick={() => setUseDark(true)}/>
+
+      <div className="app">
+        <h1>URL Shortener</h1>
+        <div className="input_container">
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            className="submit_button"
+            onClick={() => setResult(url)}
+          >
+            Submit
+          </button>
+        </div>
+        {result && <Result url={result} />}
       </div>
-      {result && <Result url={result} />}
-    </div>
+    </>
   )
 }
 
