039b5ec8af6c10477cc970b7ad2910bb1d79b660
eeren3411 <eeren3411@gmail.com>
2025-05-14 21:14:14 +0300

    Redirect route
    Co-authored-by: İdil Patir <idilpatir@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Redirect from "./pages/Redirect";
 
 function App(){
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="*" element={<Redirect />} />
 			</Routes>
 		</BrowserRouter>
 	)
